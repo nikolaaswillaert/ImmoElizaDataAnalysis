40ead37ed9e80a61da968dc5fd65232adb648874
NikolaasWillaert <nikolaas.willaert@gmail.com>
2023-07-06 11:59:22 +0200

    added correlation and heatmap

--- utils/DrawHeatmap.py
+++ utils/DrawHeatmap.py
@@ -22,8 +22,37 @@ def drawheatmap():
     heatmap_layer.add_to(belgium_map)
 
     # Save the map as an HTML file
-    belgium_map.save('belgian_price_heatmap.html')
+    belgium_map.save('belgian_heatmap_house_prices.html')
 
 
-if __name__ == "__main__":
-    drawheatmap()
+def drawfullmap():
+    df = pd.read_csv('data/cleaned.csv')
+    belgium_map = folium.Map(location=[50.5039, 4.4699], zoom_start=8)
+
+    # Iterate over each row in the dataset and add markers to the map
+    for index, row in df.iterrows():
+        latitude = row['latitude']
+        longitude = row['longitude']
+        price = row['price']
+
+        # Add a marker to the map for each location
+        folium.Marker(
+            location=[latitude, longitude],
+            popup=f"Price: {price}",
+            icon=folium.Icon(color='blue', icon='home')
+        ).add_to(belgium_map)
+
+    # Normalize the prices for better heatmap visualization
+    max_price = df['price'].max()
+    normalized_prices = df['price'] / max_price
+
+    # Add a normalized heatmap layer to the map based on the house prices
+    heatmap_layer = plugins.HeatMap(
+        df[['latitude', 'longitude']].values.tolist(),
+        min_opacity=0.3, blur=10,
+        gradient={0.4: 'blue', 0.65: 'lime', 1: 'red'},
+        scale_radius=False, radius=15, weights=normalized_prices.tolist()  # Convert Series to list
+    )
+    heatmap_layer.add_to(belgium_map)
+
+    belgium_map.save('belgian_fullmap.html')
