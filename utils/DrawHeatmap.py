4553aac9397b709658b2f05a8c1afdf457700b02
NikolaasWillaert <nikolaas.willaert@gmail.com>
2023-07-10 12:24:58 +0200

    added some images + heatmap

--- utils/DrawHeatmap.py
+++ utils/DrawHeatmap.py
@@ -56,3 +56,21 @@ def drawfullmap():
     heatmap_layer.add_to(belgium_map)
 
     belgium_map.save('belgian_fullmap.html')
+
+
+def drawheatmap_priceperm2(df):
+    belgium_map = folium.Map(location=[50.5039, 4.4699], zoom_start=8)
+
+    # Create a list of latitudes, longitudes, and prices from the dataset
+    locations = df[['latitude', 'longitude', 'price_per_area_m2']].values.tolist()
+
+    # Add a normalized heatmap layer to the map based on the house prices
+    heatmap_layer = plugins.HeatMap(
+        locations, min_opacity=0.3, blur=10,
+        gradient={0.4: 'blue', 0.65: 'lime', 1: 'red'},
+        scale_radius=False, radius=15,  # Convert Series to list
+    )
+    heatmap_layer.add_to(belgium_map)
+
+    # Save the map as an HTML file
+    belgium_map.save('belgian_heatmap_priceperm2.html')
