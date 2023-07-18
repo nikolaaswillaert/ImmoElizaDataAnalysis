1ba34694d435714f7ecebe8927cc7cf14447f07c
NikolaasWillaert <nikolaas.willaert@gmail.com>
2023-07-18 13:19:16 +0200

    added RGBoost model + split between Type house and app

--- src/model_functions.py
+++ src/model_functions.py
@@ -1,7 +1,7 @@
 import sklearn.metrics as sm
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
@@ -9,15 +9,21 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import HuberRegressor
+from sklearn.tree import DecisionTreeRegressor
+import xgboost as xgb
 
-def linear_evaluation(X_test, y_test, y_preds):
-        print(f"Mean absolute error = {round(sm.mean_absolute_error(y_test, y_preds), 2)}\n")
-        print(f"Mean squared error = {round(sm.mean_squared_error(y_test, y_preds), 2)}\n") 
-        print(f"Median absolute error = {round(sm.median_absolute_error(y_test, y_preds), 2)}\n")
-        print(f"Explain variance score = {round(sm.explained_variance_score(y_test, y_preds), 2)}\n")
-        print(f"R2 score *coefficient of Determination = {round(sm.r2_score(y_test, y_preds), 2)}\n")
-        print('\n')
-
+def linear_evaluation(X_train, X_test, y_train, y_test, y_preds, model):
+        print(f"Mean absolute error TEST = {round(sm.mean_absolute_error(y_test, y_preds), 4)}\n")
+        print(f"Mean squared error TEST = {round(sm.mean_squared_error(y_test, y_preds), 4)}\n") 
+        print(f"Median absolute error TEST = {round(sm.median_absolute_error(y_test, y_preds), 4)}\n")
+        print(f"Explain variance score TEST = {round(sm.explained_variance_score(y_test, y_preds), 4)}\n")
+        print(f"R2 score *coefficient of Determination TEST = {round(sm.r2_score(y_test, y_preds), 4)}\n")
+        print('--------------------------------------')
+        print(f'TRAINING SCORE: {model.score(X_train, y_train)}')
+        print(f'TESTING SCORE: {model.score(X_test, y_test)}')
+        print('--------------------------------------')
+        scores = cross_val_score(model, X_train, y_train, scoring='r2', cv=10)
+        print(f'Cross validation scores: \n {scores}')   
         plt.scatter(X_test['living_area'], y_test, marker='+', alpha=0.3, c='blue', label='Actual')
         plt.scatter(X_test['living_area'], y_preds, marker='o', alpha=0.3, c='red', label='Predicted')
         plt.xlabel('Living Area')
@@ -37,7 +43,7 @@ def scale_data(X_train, X_test):
 
 def train_linear_regr(X, y):
         # Scale the Numerical Data with MinMax Scaler
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=22)
 
         X_train, X_test = scale_data(X_train, X_test)
 
@@ -45,12 +51,12 @@ def train_linear_regr(X, y):
         model.fit(X_train, y_train)
         y_preds = model.predict(X_test)
 
-        linear_evaluation(X_test, y_test, y_preds)
+        linear_evaluation(X_train, X_test, y_train, y_test, y_preds, model)
 
 
 def train_knn_regr(X, y, **params):
         # Scale the Numerical Data with MinMax Scaler
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=22)
 
         X_train, X_test = scale_data(X_train, X_test)
 
@@ -58,7 +64,7 @@ def train_knn_regr(X, y, **params):
         model.fit(X_train, y_train)
         y_preds = model.predict(X_test)
         
-        linear_evaluation(X_test, y_test, y_preds)
+        linear_evaluation(X_train, X_test, y_train, y_test, y_preds, model)
 
         return model, X_train, X_test, y_train, y_test
 
@@ -67,20 +73,44 @@ def train_polynomial_regr(X, y, degree):
         steps = [('polynomial', PolynomialFeatures(degree=degree)), ('linearregres', LinearRegression())]
         pipe = Pipeline(steps)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=22)
         X_train, X_test = scale_data(X_train, X_test)
 
         pipe.fit(X_train, y_train)
         y_preds = pipe.predict(X_test)
 
-        linear_evaluation(X_test, y_test, y_preds)
+        linear_evaluation(X_train, X_test, y_train, y_test, y_preds, pipe)
 
 def train_huberregressor(X,y):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=41)
+        model = HuberRegressor(max_iter=1000)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=22)
         X_train, X_test = scale_data(X_train, X_test)
 
-        model = HuberRegressor(max_iter=1000)
         model.fit(X_train, y_train)
         y_preds = model.predict(X_test)
         
-        linear_evaluation(X_test, y_test, y_preds)
+        linear_evaluation(X_train, X_test, y_train, y_test, y_preds, model)
+
+def train_decessiontree_regression(X, y):
+        model = DecisionTreeRegressor()
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.22, random_state=22)
+        X_train, X_test = scale_data(X_train, X_test)
+
+        model.fit(X_train, y_train)
+        y_preds = model.predict(X_test)
+
+        linear_evaluation(X_train, X_test, y_train, y_test, y_preds, model)
+
+def train_XGBoost_regression(X, y):
+        model = xgb.XGBRegressor(objective="reg:squarederror")
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=22)
+        X_train, X_test = scale_data(X_train, X_test)
+
+        model.fit(X_train, y_train)
+        y_preds = model.predict(X_test)
+
+        linear_evaluation(X_train, X_test, y_train, y_test, y_preds, model)
+        
