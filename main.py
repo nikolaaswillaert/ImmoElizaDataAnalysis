d09541b13d86f311ae7cf5852504ae5bf2c2529c
NikolaasWillaert <nikolaas.willaert@gmail.com>
2023-07-20 08:58:10 +0200

    change names _ create new python

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import xgboost as xgb
+import sklearn.metrics as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import KFold, GridSearchCV
